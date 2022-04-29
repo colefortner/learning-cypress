b308dcbd0d0da57aec681afe7d7fb391d1f8f002
Cole Fortner <colefortner@gmail.com>
2022-04-28 22:00:36 -0400

    Add test to fill out signup/login forms and login a user

--- cypress/integration/sashas_list_spec.js
+++ cypress/integration/sashas_list_spec.js
@@ -34,18 +34,47 @@ describe("Localhost test", () => {
 
     cy.contains("Login/Signup").click();
   });
+});
 
-  // describe("My Sixth Test", () => {
-  //   it("clicking 'type' navigates to a new url", () => {
-  //     cy.visit("http://localhost:3000");
+describe("Fills out Signup Form", () => {
+  it("clicking 'type' navigates to a new url", () => {
+    cy.visit("http://localhost:3000");
 
-  //     cy.contains("Login/Signup").click();
+    cy.contains("Login/Signup").click();
 
-  //     cy.url().should("include", "/auth");
+    cy.url().should("include", "/auth");
 
-  //     cy.get("email")
-  //       .type("fake@email.com")
-  //       .should("have.value", "fake@email.com");
-  //   });
-  // });
+    // look into how to use cypress with styled components instead of classes
+    cy.get("#username").type("emma").should("have.value", "emma");
+
+    cy.get("#email")
+      .type("fake@email.com")
+      .should("have.value", "fake@email.com");
+
+    cy.get("#password").type("aaaaaaaa").should("have.value", "aaaaaaaa");
+    cy.get("#confirm-password")
+      .type("aaaaaaaa")
+      .should("have.value", "aaaaaaaa");
+  });
+});
+
+describe("Logs in a user", () => {
+  it("clicking 'type' navigates to a new url", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.contains("Login/Signup").click();
+
+    cy.contains("Switch to Login").click();
+
+    cy.url().should("include", "/auth");
+
+    // look into how to use cypress with styled components instead of classes
+    cy.get("#email")
+      .type("kate@kate.com")
+      .should("have.value", "kate@kate.com");
+
+    cy.get("#password").type("aaaaaaaa").should("have.value", "aaaaaaaa");
+
+    cy.get("button").contains("Login").click();
+  });
 });
